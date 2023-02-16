4ed35df728088fd29304b18d13466686a3121796
MatrixMuto <wang.qiang.mu@gmail.com>
2023-02-16 19:51:54 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,26 +1,32 @@
 /**
- * The preload script runs before. It has access to web APIs
- * as well as Electron's renderer process modules and some
- * polyfilled Node.js functions.
- * 
- * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ * This file is loaded via the <script> tag in the index.html file and will
+ * be executed in the renderer process for that window. No Node.js APIs are
+ * available in this process because `nodeIntegration` is turned off and
+ * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
+ * to expose Node.js functionality from the main process.
  */
 
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+document.getElementById("btn1").onclick = () => {
+    window.electron.startDrag('drag-and-drop.md')
+}
+
+document.getElementById("btn-search").onclick = () => {
+    var ele = document.getElementById("input-search")
+    console.log(ele.value)
+    window.electron.search(ele.value)
+}
+
+const counter = document.getElementById('text1')
+
+window.electron.onUpdateCounter((event, value) => {
+    // const oldValue = Number(counter.innerText)
+    // const newValue = oldValue + value
+    counter.innerText = value
+    console.log("value=", value, "12345")
+    //event.sender.send('counter-value', newValue)
 })
 
-const { contextBridge, ipcRenderer, ipcMain } = require('electron')
-
-contextBridge.exposeInMainWorld('electron', {
-  onUpdateCounter: (callback) => ipcRenderer.on('sendText', callback),
-  startDrag: (fileName) => { ipcRenderer.send('file-drop', fileName) },
-  search: (keystr) => {ipcRenderer.send('search', keystr)}
+window.addEventListener('scroll',function(event, data){ 
+    console.log("scroll 111", event, data)
 })
